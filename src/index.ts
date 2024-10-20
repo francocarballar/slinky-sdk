800a11cda5822196cd629b097caee1b40ce277f5
Franco Carballar <francocarballar@gmail.com>
2024-10-20 04:49:48 -0300

    feat: :sparkles: actualiza las exportaciones dentro del index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { generateMessageObjectsFromMetadata } from './utils/generateMessageObjectsFromMetadata'
 import { createMetadata } from './utils/createMetadata'
+import { extractTransactionArguments } from './utils/extractTransactionArguments'
+import { generateTransactionData } from './utils/generateTransactionData'
 
-export { generateMessageObjectsFromMetadata, createMetadata }
+export {
+	generateMessageObjectsFromMetadata,
+	createMetadata,
+	extractTransactionArguments,
+	generateTransactionData
+}
