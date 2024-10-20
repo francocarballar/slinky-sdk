6b8a7901feacdf5b1607724983e2e2244218944b
Franco Carballar <francocarballar@gmail.com>
2024-10-20 06:00:37 -0300

    feat: :label: actualiza las interfaces y crea nuevas en blockchainAction

--- src/interfaces/blockchainAction.d.ts
+++ src/interfaces/blockchainAction.d.ts
@@ -1,7 +1,23 @@
+export interface ContractABI {
+	inputs: Array<{
+		internalType: string
+		name: string
+		type: string
+	}>
+	outputs: Array<{
+		internalType?: string
+		name?: string
+		type: string
+	}>
+	stateMutability: 'view' | 'pure' | 'nonpayable' | 'payable'
+	type: 'function' | 'constructor' | 'event' | 'fallback'
+	name: string
+}
+
 export interface BlockchainAction {
 	label: string
 	contractAddress: `0x${string}`
-	contractABI: object[]
+	contractABI: ContractABI[]
 	transactionParameters: BlockchainParameter[]
 	blockchainActionType: 'read' | 'write'
 	functionName: string
