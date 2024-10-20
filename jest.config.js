2b85866e2b3c1bd747cb41df4d329f8b4fe62efb
Franco Carballar <francocarballar@gmail.com>
2024-10-19 21:24:47 -0300

    fix: :bug: resuleve problemas o lo intenta

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-        '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1'
-    }
-};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createMetadata = exports.generateMessageObjectsFromMetadata = void 0;
-var generateMessageObjectsFromMetadata_1 = require("./utils/generateMessageObjectsFromMetadata");
-Object.defineProperty(exports, "generateMessageObjectsFromMetadata", { enumerable: true, get: function () { return generateMessageObjectsFromMetadata_1.generateMessageObjectsFromMetadata; } });
-var createMetadata_1 = require("./utils/createMetadata");
-Object.defineProperty(exports, "createMetadata", { enumerable: true, get: function () { return createMetadata_1.createMetadata; } });
